40696ce0830915f0828f08d398c72178e52a1a6f
Seth Bonnie <seth@lifecorp.com>
2015-10-06 17:26:23 -0500

    Add some docs explaining each task

--- gulpfile.js
+++ gulpfile.js
@@ -9,11 +9,24 @@ var source = require( 'vinyl-source-stream' );
 var plumber = require( 'gulp-plumber' );
 var browserify = require( 'browserify' );
 
+/**
+  * Main source paths for easy access. Long patterns, especially of
+  * files in node_modules/ are better off here. Destinations should be
+  * in the source, since those are usually specific to a task and not
+  * usually shareable. It's also more readable to have exact paths with
+  * destinations.
+  */
 var paths = {
 	cssSrc: ['src/**/*.css', 'src/**/*.less'],
 	jsSrc: ['src/**/*.js']
 };
 
+
+/**
+  * After we have babelfied our sources, we browserify them
+  * starting from the AppContainer and dump them into `public/js/`
+  * for all the world to consume.
+  */
 gulp.task( 'js', ['babel'], function() {
 	var b = browserify({
     entries: './build/js/AppContainer.js',
@@ -25,15 +38,25 @@ gulp.task( 'js', ['babel'], function() {
     .pipe( source('bundle.js') )
     .pipe( buffer() )
     .pipe( gulp.dest('public/js/') );
-});
+});// /gulp.task('js')
 
+
+/**
+  * Process all our JavaScript sources through babel and dump
+  * them into the `build/` folder.
+  */
 gulp.task( 'babel', function() {
 	return gulp.src( paths.jsSrc )
 		.pipe( plumber() )
 		.pipe( babel() )
 		.pipe( gulp.dest('build/') );
-});
+});// /gulp.task('babel')
 
+
+/**
+  * Process all our LESS and CSS files through less and drop them into
+  * the `public/css/` directory.
+  */
 gulp.task( 'css', function() {
 	return gulp.src( paths.cssSrc )
 		.pipe( plumber() )
@@ -44,15 +67,21 @@ gulp.task( 'css', function() {
     .pipe( gulp.dest('public/css/') );		
 });
 
+
 /**
   * This task is for tasks that bring in external dependencies, for
-  * example, copying bootstrap files to `public/lib`
+  * example, copying bootstrap files to `public/lib`. So the dependencies
+  * array will be filled with the various tasks we use to copy those files
+  * to the the lib directory, e.g.:
+  * 	gulp.task('lib',['bootstrap', 'font-awesome']);
   */
 gulp.task( 'lib', []);
 
+
 gulp.task( 'default', ['js', 'css', 'lib', 'watch']);
+
 
 gulp.task( 'watch', function() {
 	gulp.watch(paths.jsSrc, ['js']);
 	gulp.watch(paths.cssSrc, ['css']);
-});
+});// /gulp.task('watch')
